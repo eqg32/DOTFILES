a2ba971d4f351604fb5e1bcc1c96dbde2b9a4b1a
eqg32 <sobakpes94@ya.ru>
2025-04-26 00:03:37 +0300

    Set up emacs server

--- .emacs.d/elisp/faces.el
+++ .emacs.d/elisp/faces.el
@@ -1,18 +1,18 @@
-(when (or (memq 'ewal-doom-one custom-enabled-themes)
-	  (memq 'ewal-doom-vibrant custom-enabled-themes))
-  (set-face-attribute 'company-tooltip nil
-		      :foreground (face-attribute 'font-lock-comment-face :foreground))
-  (set-face-attribute 'company-tooltip-selection nil
-		      :foreground (face-attribute 'company-tooltip-common :foreground)
-		      :background (face-attribute 'default :background))
-  (set-face-attribute 'line-number nil :foreground (face-attribute 'font-lock-comment-face :foreground)))
-
-(set-frame-parameter nil 'alpha-background 90)
-
-(set-face-attribute 'variable-pitch nil :font "Iosevka Aile" :height 130)
-
-(set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 130)
-
 (with-eval-after-load 'org
   (set-face-attribute 'org-level-1 nil :height 1.4)
   (set-face-attribute 'org-level-2 nil :height 1.2))
+
+(defun setup-frame ()
+  (when (or (memq 'ewal-doom-one custom-enabled-themes)
+	    (memq 'ewal-doom-vibrant custom-enabled-themes))
+    (set-face-attribute 'company-tooltip nil
+			:foreground (face-attribute 'font-lock-comment-face :foreground))
+    (set-face-attribute 'company-tooltip-selection nil
+			:foreground (face-attribute 'company-tooltip-common :foreground)
+			:background (face-attribute 'default :background))
+    (set-face-attribute 'line-number nil :foreground (face-attribute 'font-lock-comment-face :foreground)))
+  (set-frame-parameter nil 'alpha-background 90)
+  (set-face-attribute 'variable-pitch nil :font "Iosevka Aile" :height 130)
+  (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 130))
+
+(add-hook 'server-after-make-frame-hook 'setup-frame)
---
+++ .emacs.d/elisp/server.el
@@ -0,0 +1,1 @@
+(add-hook 'server-after-make-frame-hook 'ibuffer)
