30345f416718085344f7f733d0d7830f37271a87
eqg32 <sobakpes94@ya.ru>
2025-04-25 22:38:56 +0300

    C changes

--- .emacs.d/elisp/c.el
+++ .emacs.d/elisp/c.el
@@ -1,4 +1,5 @@
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '(c-mode . ("clangd"))))
-  ;; (add-to-list 'eglot-server-programs '(c-ts-mode . ("clangd"))))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.c\\'" . c-ts-mode))
+;; (with-eval-after-load 'eglot
+;;   (add-to-list 'eglot-server-programs '(c-mode . ("clangd"))))
+;;   ;; (add-to-list 'eglot-server-programs '(c-ts-mode . ("clangd"))))
