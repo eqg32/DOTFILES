58b2cf6b9a53e8db2865a95d10737b80d11bc32d
eqg32 <sobakpes94@ya.ru>
2025-05-02 23:57:44 +0300

    Changed hooks for listen.el

--- .emacs.d/packages/use-listen.el
+++ .emacs.d/packages/use-listen.el
@@ -1,4 +1,6 @@
 (use-package listen
   :ensure t
+  :hook ((listen-queue-mode . evil-emacs-state)
+	 (listen-library-mode . evil-emacs-state))
   :custom
   (listen-lighter-format "%s %a: %t (%r)%E"))
