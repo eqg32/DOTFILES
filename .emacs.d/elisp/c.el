8d20438148a151fcda05f0ec0ca0a041eef8e41c
eqg32 <sobakpes94@ya.ru>
2025-05-02 23:21:22 +0300

    Deleted c.el

--- .emacs.d/elisp/c.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.c\\'" . c-ts-mode))
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs '(c-mode . ("clangd"))))
-;;   ;; (add-to-list 'eglot-server-programs '(c-ts-mode . ("clangd"))))
