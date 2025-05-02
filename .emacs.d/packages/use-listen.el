3859e29327fdd91d25e4ff5e8a43e06fb0d1df7f
eqg32 <sobakpes94@ya.ru>
2025-05-02 23:25:39 +0300

    New packages

---
+++ .emacs.d/packages/use-listen.el
@@ -0,0 +1,4 @@
+(use-package listen
+  :ensure t
+  :custom
+  (listen-lighter-format "%s %a: %t (%r)%E"))
---
+++ .emacs.d/packages/use-outline-indent.el
@@ -0,0 +1,3 @@
+(use-package outline-indent
+  :ensure t
+  :hook (prog-mode . outline-indent-minor-mode))
