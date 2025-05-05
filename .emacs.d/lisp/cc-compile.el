59ccb46be93a3507e3cc0b1229603bbef89d24b8
eqg32 <sobakpes94@ya.ru>
2025-05-05 22:56:46 +0300

    Changed C and C++ compilation function

--- .emacs.d/lisp/cc-compile.el
+++ .emacs.d/lisp/cc-compile.el
@@ -1,27 +1,34 @@
-(defun user/c-compile ()
-  "Function to compile main.c. Not meant to be run anywhere else"
+(defun user/cc-compile ()
+  "Function to compile main file"
   (interactive)
-  (if (and (file-directory-p "src") (file-directory-p "include"))
-      (shell-command "gcc main.c src/* -Iinclude. -o main")
-    (shell-command "gcc main.c -o main")))
+  (cond
+   ((file-exists-p "main.c") (funcall (lambda ()
+					(if (and (file-directory-p "src") (file-directory-p "include"))
+					    (shell-command "gcc main.c src/* -Iinclude -o main")
+					  (shell-command "gcc main.c -o main")))))
+   ((file-exists-p "main.cpp") (funcall (lambda ()
+					(if (and (file-directory-p "src") (file-directory-p "include"))
+					    (shell-command "gcc main.cpp src/* -Iinclude -o main")
+					  (shell-command "gcc main.cpp -o main")))))
+   (t (message "No file exists."))))
 
-(defun user/c++-compile ()
-  "Function to compile main.cpp. Not meant to be run anywhere else"
+(defun user/cc-run ()
+  "Function to run main"
   (interactive)
-  (if (and (file-directory-p "src") (file-directory-p "include"))
-      (shell-command "gcc main.cpp src/* -Iinclude. -o main")
-    (shell-command "gcc main.cpp -o main")))
+  (if (file-exists-p "main")
+      (shell-command "./main")
+    (message "No file exists.")))
 
-(defun user/c-compile-and-run ()
-  "Function to compile and run main.c. Not meant to be run anywhere else"
-  (interactive)
-  (if (and (file-directory-p "src") (file-directory-p "include"))
-      (shell-command "gcc main.c src/* -Iinclude. -o main && ./main")
-    (shell-command "gcc main.c -o main && ./main")))
+(defun user/cc-run-with-input (input)
+  "Function to run main"
+  (interactive "sInput: ")
+  (if (file-exists-p "main")
+      (shell-command (format "echo %s | ./main" input))
+    (message "No file exists.")))
 
-(defun user/c++-compile-and-run ()
-  "Function to compile and run main.cpp. Not meant to be run anywhere else"
-  (interactive)
-  (if (and (file-directory-p "src") (file-directory-p "include"))
-      (shell-command "gcc main.cpp src/* -Iinclude. -o main && ./main")
-    (shell-command "gcc main.cpp -o main && ./main")))
+(defun user/cc-run-with-args (args)
+  "Function to run main"
+  (interactive "sInput: ")
+  (if (file-exists-p "main")
+      (shell-command (format "./main %s" args))
+    (message "No file exists.")))
