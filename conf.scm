36ed2999a85fded3e21b3328fdb8e6e852cc2718
ivan gnomov <sobakpes94@ya.ru>
2025-08-06 09:27:19 +0000

    Changed packages definitions

--- conf.scm
+++ conf.scm
@@ -1,21 +1,17 @@
 (use-modules (gnu services)
 	     (gnu home services)
 	     (gnu home services dotfiles)
-	     (gnu packages)
-	     (gnu packages emacs)
-	     (gnu packages terminals)
-     	     (gnu packages wm) (gnu packages linux) (gnu packages rust-apps) (gnu packages xdisorg) (gnu packages python-xyz)
-	     (gnu packages mpd) (gnu packages pulseaudio))
+	     (gnu packages))
 
 (home-environment
  (packages
-  (list
-   emacs-pgtk
-   kitty
-   swayfx i3status-rust bemenu i3-autotiling
-   grimshot wl-clipboard
-   brightnessctl
-   mpd mpd-mpc pulseaudio pulsemixer))
+  (specifications->packages
+   '("emacs-pgtk"
+     "kitty"
+     "swayfx" "i3status-rust" "bemenu" "i3-autotiling"
+     "grimshot" "wl-clipboard"
+     "brightnessctl"
+     "mpd" "mpd-mpc" "pulseaudio" "pulsemixer")))
  (services
   (list
    (service home-dotfiles-service-type
