e08126ea2855bfd2d1101b2f1c031c60e7b25313
eqg32 <sobakpes94@ya.ru>
2026-08-21 10:40:38 +0300

    Added melpa back

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,7 @@
 (add-elements-to-list
  package-archives
  '("gnu" . "https://elpa.gnu.org/packages/")
+ '("melpa" . "https://melpa.org/packages/")
  '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (setopt package-enable-at-startup nil)
